fca3f6635c0dc00cdd96a6f1d03d9bc63e698b72
Adrian bc2024-1 <mrsaisyt@gmail.com>
2024-11-24 22:18:30 +0200

    glfix2

--- index.js
+++ index.js
@@ -14,23 +14,4 @@ if (!fs.existsSync(options.input)) {
   console.error('Cannot find input file');
   process.exit(1);
 }
-let data;
-try {
-  data = fs.readFileSync(options.input, 'utf-8');
-} catch (err) {
-  console.error('Error reading the input file:', err.message);
-  process.exit(1);
-}
-if (!options.output && !options.display) {
-  process.exit(0); 
-}
 
-const result = "This is a test result"; 
-if (options.output) {
-  try {
-    fs.writeFileSync(options.output, result, 'utf-8');
-    console.log(`Result written to ${options.output}`);
-  } catch (err) {
-    console.error('Error writing to output file:', err.message);
-  }
-}
